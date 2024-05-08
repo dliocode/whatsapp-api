74b8056e4c2c3df744e1b7ef3802d7bfe864ce15
Danilo Lucas <developer.dlio@gmail.com>
2024-05-08 09:30:01 -0300

    fix: validate webhook

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ require('dotenv').config()
 const port = process.env.PORT || 3000
 
 // Check if BASE_WEBHOOK_URL environment variable is available
-if (!baseWebhookURL) {
-  console.error('BASE_WEBHOOK_URL environment variable is not available. Exiting...')
-  process.exit(1) // Terminate the application with an error code
-}
+// if (!baseWebhookURL) {
+//   console.error('BASE_WEBHOOK_URL environment variable is not available. Exiting...')
+//   process.exit(1) // Terminate the application with an error code
+// }
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
